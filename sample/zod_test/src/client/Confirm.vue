db6bd99809be8319d29d4721467edc762c8d1460
_ñ <knaka0209@gmail.com>
2024-05-20 18:49:08 +0900

    up, 0520a8

---
+++ sample/zod_test/src/client/Confirm.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import Crud from './Zod/Crud';
+import Validate from './Zod/Validate';
+import DialogBox from '../components/DialogBox.vue'
+import ConfirmDialog from '../components/ConfirmDialog.vue'
+//
+import { ref, reactive } from 'vue';
+//
+const testProc = async function(){
+  const dlg = document.getElementById('confirmDialog');
+  if(dlg) {
+    //@ts-ignore
+    dlg.showModal();
+  }
+//console.log("errors.title=", errors.title);
+}
+//
+const cbFunc = async function(){
+  console.log("#parent.cbFunc");
+  alert("parent.cbFunc");
+}
+</script>
+
+<!-- template -->
+<template>
+  <div class="container mx-auto my-2 px-8 bg-white">
+    <h1 class="text-4xl text-gray-700 font-bold my-2">Confirm.vue! </h1>
+    <hr class="my-2" />
+    <button @click="testProc" class="btn">Test</button>
+    <!-- dialog -->
+    <ConfirmDialog message="OK? next" @cbFunction="cbFunc" />
+  </div>
+</template>
+
+<!-- style -->
+<style scoped>
+</style>
+
+<!-- 
+-->
---
+++ sample/zod_test/src/components/ConfirmDialog.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps(['message']);
+const emit = defineEmits(['cbFunction'])
+//
+const closeButton = function(){
+  const dlg = document.getElementById('confirmDialog');
+  if(dlg) {
+    //@ts-ignore
+    dlg.close();
+  }
+}
+//
+const okFunc = async function(){
+  console.log("#okFunc");
+  const dlg = document.getElementById('confirmDialog');
+  if(dlg) {
+    //@ts-ignore
+    dlg.close();
+    emit('cbFunction');
+  }
+}
+</script>
+
+<template>
+  <dialog id="confirmDialog" class="dialog">
+    <div class="bg-white px-8 pt-3 pb-3 dialog_body_wrap">
+      <p class="text-3xl font-bold text-gay-900">{{ message }}</p>
+      <hr class="my-3" />
+      <button 
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-8 me-2 rounded"
+      type="submit" @click="okFunc">Ok</button>
+      <button 
+      class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-4 border border-blue-500 hover:border-transparent rounded"
+      type="submit"  @click="closeButton">Cancel</button>
+    </div>
+  </dialog>
+</template>
+
+<style scoped>
+</style>
+<!--
+s-->
